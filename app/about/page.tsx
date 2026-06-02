2eccbd320102427ee9e243c867ec33424989eb5b
sh3ki <shekaigarcia@gmail.com>
2026-06-03 02:21:08 +0800

    Update app/about/page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import AboutClient from './AboutClient'
 import { getPublicTeam } from '@/lib/server/public-data'
 
-export const revalidate = 3600
+export const revalidate = false
 
 export const metadata: Metadata = {
   title: 'About Us',
