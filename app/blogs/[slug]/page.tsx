95105b6863973b28afb82f97ca155ce2d3edb6c8
sh3ki <shekaigarcia@gmail.com>
2026-06-03 02:21:08 +0800

    Update app/blogs/[slug]/page.tsx

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -3,7 +3,7 @@ import { notFound } from 'next/navigation'
 import BlogPostClient from './BlogPostClient'
 import { getPublicBlogs } from '@/lib/server/public-data'
 
-export const revalidate = 3600
+export const revalidate = false
 
 interface Props {
   params: Promise<{ slug: string }>
